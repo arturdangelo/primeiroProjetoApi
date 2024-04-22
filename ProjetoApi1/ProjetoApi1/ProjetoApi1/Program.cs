76bab9f293447799dbbd2f135e1bdd46bc43e3bd
Artur D Angelo <arturdangelo@gmail.com>
2024-04-21 22:19:29 -0300

    Arrumando a Api

--- ProjetoApi1/ProjetoApi1/ProjetoApi1/Program.cs
+++ ProjetoApi1/ProjetoApi1/ProjetoApi1/Program.cs
@@ -16,6 +16,15 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Adiciona suporte ao CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSpecificOrigin",
+        builder => builder.WithOrigins("http://127.0.0.1:5500")
+                          .AllowAnyMethod()
+                          .AllowAnyHeader());
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -26,6 +35,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+// Aplicar a política CORS
+app.UseCors("AllowSpecificOrigin");
 
 app.UseAuthorization();
 
